c79a8971eda876d1dac48fab955565ad11b17227
hyeonheee <130201520+hyeonheee@users.noreply.github.com>
2023-07-03 21:37:28 +0900

    Time: 2 ms (100.00%), Space: 43.1 MB (75.76%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -2,12 +2,13 @@ class Solution {
     public int maxAreaOfIsland(int[][] grid) {
 
         int maxArea = 0;
-        int row=grid.length;
-        int col=grid[0].length;
+        int row=grid.length; // 행 -
+        int col=grid[0].length; // 열 ㅣ
 
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
                 maxArea = Math.max(maxArea, findArea(grid, i, j));
+                //maxArea 와 findArea중 큰 값을 maxArea에 저장
             }
         }
         return maxArea;
