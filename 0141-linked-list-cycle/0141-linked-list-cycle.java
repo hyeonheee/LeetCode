e573c73eac6c54812c0c42bb6a9542e448dfbf0d
hyeonheee <130201520+hyeonheee@users.noreply.github.com>
2023-07-07 14:18:01 +0900

    Time: 0 ms (100.00%), Space: 43.8 MB (38.05%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -13,17 +13,17 @@ public class Solution {
     public boolean hasCycle(ListNode head){
        if(head==null) return false;
        
-       ListNode fast=head.next;
-       ListNode slow=head;
+       ListNode fast=head.next; // 한번에 두 칸 이동
+       ListNode slow=head; // 한번에 한 칸 이동
        
        while (slow!=fast){
            if(fast==null || fast.next==null) {
                return false;
            }
            slow=slow.next;
-           fast=fast.next.next;
+           fast=fast.next.next; // 두 칸 이동
        }
-       return true;
+       return true; // slow == fast 
     }
 
 }
