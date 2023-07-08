8fdfdcb06bbb4d5f49a4d535156617473da9e4f5
hyeonheee <130201520+hyeonheee@users.noreply.github.com>
2023-07-08 14:43:08 +0900

    Time: 4 ms (98.44%), Space: 41.6 MB (71.97%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,18 +1,18 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        HashMap<Character, Character> dicionary = new HashMap<Character, Character>();
-        for (int i = 0; i < s.length(); i++) {
-            char sc = s.charAt(i);
-            char tc = t.charAt(i);
 
-            if (dicionary.containsKey(sc)) {
-                if (tc != dicionary.get(sc))
-                    return false;
-            } else if (dicionary.containsValue(tc)) {
+        int map1[] = new int[200];
+        int map2[] = new int[200];
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (map1[s.charAt(i)] != map2[t.charAt(i)]) {
                 return false;
-            } else {
-                dicionary.put(sc,tc);
             }
+            map1[s.charAt(i)] = i + 1;
+            map2[t.charAt(i)] = i + 1;
         }
         return true;
     }
