f8737554b4ddbaf92e3bbf468c0ccc90c0e0215b
hyeonheee <130201520+hyeonheee@users.noreply.github.com>
2023-07-12 10:21:16 +0900

    Time: 0 ms (100.00%), Space: 44.3 MB (38.49%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,25 +1,28 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
 
-        if(image[sr][sc]==color)
+
+        int startColor = image[sr][sc];
+        // 시작값이 color와 동일하면 변경x
+        if (startColor == color)
             return image;
-        
-        int startColor=image[sr][sc];
-        
-        fill(image,sr,sc,startColor,color);
+
+        fill(image, sr, sc, startColor, color);
         return image;
     }
 
-    public void fill(int [][] image, int sr, int sc, int startColor, int Color){
-        if(sr<0||sc<0||sr>=image.length||sc>=image[0].length||image[sr][sc]!=startColor){
+    public void fill(int[][] image, int sr, int sc, int startColor, int Color) {
+        // 더 이상 검증할 수 없을 때
+        if (sr < 0 || sc < 0 || sr >= image.length || sc >= image[0].length || image[sr][sc] != startColor) {
             return;
         }
 
-        image[sr][sc]=Color;
+        //시작 위치의 값이 newColor와 다르면 시작 위치의 값 업데이트
+        image[sr][sc] = Color;
 
-        fill(image,sr-1,sc,startColor,Color);
-        fill(image,sr+1,sc,startColor,Color);
-        fill(image,sr,sc-1,startColor,Color);
-        fill(image,sr,sc+1,startColor,Color);
+        fill(image, sr - 1, sc, startColor, Color);
+        fill(image, sr + 1, sc, startColor, Color);
+        fill(image, sr, sc - 1, startColor, Color);
+        fill(image, sr, sc + 1, startColor, Color);
     }
 }
