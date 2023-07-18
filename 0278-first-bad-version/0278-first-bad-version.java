0648eed50b22878a976e68ad7165dec83328cbb6
hyeonheee <130201520+hyeonheee@users.noreply.github.com>
2023-07-18 21:14:45 +0900

    Time: 16 ms (96.92%), Space: 39.2 MB (80.65%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -10,12 +10,12 @@ public class Solution extends VersionControl {
         
         while(start<end){
             mid=start+(end-start)/2; //mid를 계산하는 공식
-            if(isBadVersion(mid))
-                end=mid;
-            else
-                start=mid+1;
+            if(isBadVersion(mid)) //현재 mid가 잘못된 제품인 경우
+                end=mid; //mid가 최초 잘못된 제품이거나, mid 이전에 잘못된 제품이 있을 수 있음
+            else // 현재 mid가 올바른 제품인 경우
+                start=mid+1; // mid 이후에 잘못된 버전이 있음
             
         }
-        return start;
+        return start; 
     }
 }
