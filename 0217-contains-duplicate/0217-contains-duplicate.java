582e7d3243ba8758e44b7d42ccbdc0e2739b3c30
hyeonheee <130201520+hyeonheee@users.noreply.github.com>
2023-07-19 09:20:01 +0900

    Time: 12 ms (83.24%), Space: 55.5 MB (54.25%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -3,12 +3,27 @@ class Solution {
 
         HashSet<Integer> hashSet = new HashSet<>();
 
-        for (int num : nums) {
-            if (!hashSet.add(num)) {
-                return true;
+        for(int i=0; i<nums.length; i++){
+            if(hashSet.contains(nums[i])){
+                return  true;
             }
-            hashSet.add(num);
+            hashSet.add(nums[i]);
         }
         return false;
     }
 }
+
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+
+//         HashSet<Integer> hashSet = new HashSet<>();
+
+//         for (int num : nums) {
+//             if (!hashSet.add(num)) {
+//                 return true;
+//             }
+//             hashSet.add(num);
+//         }
+//         return false;
+//     }
+// }
