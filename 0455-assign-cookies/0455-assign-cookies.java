ff9c0ae5d0fb0ad61f82406a56d39ce8478a0f6f
hyeonheee <130201520+hyeonheee@users.noreply.github.com>
2023-07-22 22:50:16 +0900

    Time: 30 ms (6.92%), Space: 44.5 MB (57.82%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+
+     int content=0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        
+        int find=0;
+        for (int i = 0; i < s.length; i++) {
+            for (int j = find; j <g.length ; j++) {
+                if(s[i]>=g[j]){
+                    content++;
+                    find=j+1;
+                    break;
+                }
+            }
+        }
+        return content;
+    }
+}
